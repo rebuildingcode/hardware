251f637e1a13a6b2777b51a07d3693cb0c609ce0
Nguyen Ngo <mnguyenngo@gmail.com>
2019-10-04 23:52:40 -0700

    Try using find_packages

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='rbc',
-      version='0.1dev',
-      description='An object library for advanced design and construction',
-      author='Nguyen Ngo',
-      author_email='mnguyenngo@gmail.com',
-      url='https://github.com/rebuildingcode/rbc',
-      packages=['rbc'],
-     )
+setup(
+    name='rbc',
+    version='0.1dev',
+    description='An object library for advanced design and construction',
+    author='Nguyen Ngo',
+    author_email='mnguyenngo@gmail.com',
+    url='https://github.com/rebuildingcode/rbc',
+    package_dir={'': 'rbc'},
+    packages=find_packages(where='rbc'),
+)
