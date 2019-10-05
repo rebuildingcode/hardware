1ea392aa927bd0c32fa8b1019ba9ce75f86bea2c
Nguyen Ngo <mnguyenngo@gmail.com>
2019-10-04 23:01:19 -0700

    Make rbc installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='rbc',
+      version='0.1dev',
+      description='An object library for advanced design and construction',
+      author='Nguyen Ngo',
+      author_email='mnguyenngo@gmail.com',
+      url='https://github.com/rebuildingcode/rbc',
+      packages=['src'],
+     )
