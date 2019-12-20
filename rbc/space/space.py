59c9f32ea9ad3ad1711cf71be1fad79dbe662110
Nguyen Ngo <mnguyenngo@gmail.com>
2019-12-19 17:41:34 -0800

    100% coverage for Space

--- rbc/space/space.py
+++ rbc/space/space.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+
 from shapely.geometry import Polygon
 
 from ..point import Point
@@ -22,6 +23,7 @@ class Space:
     """
 
     def __init__(self, points=None, name=None, contents=None):
+        self.max_retries = 15
         self.name = name
         self.contents = contents
         self.polygon = Polygon(shell=[(pt.x, pt.y) for pt in points])
@@ -33,30 +35,118 @@ class Space:
 
     @property
     def area(self):
+        """"""
         return self.polygon.area
 
     def place_contents(self, contents):
+        """"""
         for content in contents:
             self.place_content(content)
 
-    def place_content(self, content):
+    def place_content(self, content, retries=0):
+        """Places content in a valid location"""
+        if hasattr(content, 'name'):
+            # get label for polygon-type objects with name attribute
+            plan_label = content.name
+        else:
+            # otherwise default to using the area value as the label
+            # this supports shapely's Polygon object
+            plan_label = f"AREA: {content.area}"
+
+        if isinstance(content, Space):
+            content_poly = content.polygon
+        else:
+            content_poly = content
+
+        # TODO: add capability for Space to remember failed attempts to reduce
+        # amount of tries needed; possibly keep a list of failed locations
+        retries = 0
+        while retries < self.max_retries:
+            if retries < self.max_retries // 2:
+                # attempt to locate object at the corners with the first half
+                # of tries
+                potential_location = self.corner_locate(content_poly)
+            else:
+                # attempt to locate object at the edge with the rest of the
+                # tries
+                potential_location = self.edge_locate(content_poly)
+
+            # create a polygon with the potential_location points to define
+            # the content's place
+            place = Polygon(shell=potential_location)
+
+            if self.validate_place(place):
+                # object's location is valid; add object to self.plan dict
+                self.plan[plan_label] = place
+                return
+            else:  # location is not valid; retry
+                print(f"Failed to place {plan_label}. Retrying...")
+                retries += 1
+
+        print(f"Reached max retries for {plan_label}. Skipping...")
+        # max retries reached; continue finishing up Space construction
+        # without content
+        return
+
+    def validate_place(self, place):
+        """A place is valid if it is not in conflict with any other object in
+        self.plan
+        """
+        if len(self.plan) > 0:
+            checks = []
+            for thing in self.plan.values():
+                checks.append(not place.overlaps(thing) and
+                              not place.within(thing) and
+                              not thing.within(place))
+            if all(checks) == True:
+                return True
+            else:  # retry place
+                return False
+        else:
+            return True
+
+    def corner_locate(self, content):
+        """Return a set of points for a random corner"""
         b_x_min, b_y_min, b_x_max, b_y_max = self.polygon.bounds
         c_x_min, c_y_min, c_x_max, c_y_max = content.bounds
         x_min = b_x_min + c_x_min
         x_max = b_x_max - c_x_max
-        y_min = b_y_min - c_y_min
+        y_min = b_y_min + c_y_min
         y_max = b_y_max - c_y_max
 
-        rand_x = random.randint(x_min, x_max)
-        rand_y = random.randint(y_min, y_max)
+        rand_x = random.choice([x_min, x_max])
+        rand_y = random.choice([y_min, y_max])
 
-        new_points = [
+        loc_points = [
             (x+rand_x, y+rand_y) for x, y in content.exterior.coords[:-1]
         ]
 
-        k = content.name
+        return loc_points
 
-        self.plan[k] = Polygon(shell=new_points)
+
+    def edge_locate(self, content):
+        """Return a set of points for a random location at the edge"""
+        b_x_min, b_y_min, b_x_max, b_y_max = self.polygon.bounds
+        c_x_min, c_y_min, c_x_max, c_y_max = content.bounds
+        x_min = b_x_min + c_x_min
+        x_max = b_x_max - c_x_max
+        y_min = b_y_min + c_y_min
+        y_max = b_y_max - c_y_max
+
+        first_axis = random.choice(['x', 'y'])
+
+        if first_axis is 'x':
+            rand_x = random.choice([x_min, x_max])
+            rand_y = random.randint(y_min, y_max)
+        else:
+            rand_y = random.choice([y_min, y_max])
+            rand_x = random.randint(x_min, x_max)
+
+        loc_points = [
+            (x+rand_x, y+rand_y) for x, y in content.exterior.coords[:-1]
+        ]
+
+        return loc_points
 
 
     def plot(self, figsize=(12, 12)):
---
+++ rbc/space/test_space.py
@@ -0,0 +1,123 @@
+import pytest
+from unittest import mock
+
+from .space import Space
+from ..point.point import Point
+from shapely.geometry import Polygon
+
+
+# =================
+# FIXTURES
+# =================
+
+@pytest.fixture
+def ten_by_ten_points():
+    p1 = Point(0, 0, 0)
+    p2 = Point(0, 10, 0)
+    p3 = Point(10, 10, 0)
+    p4 = Point(10, 0, 0)
+
+    yield [p1, p2, p3, p4]
+
+
+@pytest.fixture
+def five_by_four_points():
+    p1 = Point(0, 0, 0)
+    p2 = Point(0, 4, 0)
+    p3 = Point(5, 4, 0)
+    p4 = Point(5, 0, 0)
+
+    yield [p1, p2, p3, p4]
+
+@pytest.fixture
+def ten_by_ten_space(ten_by_ten_points):
+    yield Space(points=ten_by_ten_points, name='10x10 Space')
+
+
+@pytest.fixture
+def five_by_four_poly(five_by_four_points):
+    poly = Polygon(shell = [(pt.x, pt.y) for pt in five_by_four_points])
+
+    yield poly
+
+
+@pytest.fixture
+def five_by_four_space(five_by_four_points):
+    poly = Space(points=five_by_four_points, name='5x4 Space')
+
+    yield poly
+
+
+# =================
+# HELPER FUNCTIONS
+# =================
+
+def nameable_space(name, x, y):
+    p1 = Point(0, 0, 0)
+    p2 = Point(0, y, 0)
+    p3 = Point(x, y, 0)
+    p4 = Point(x, 0, 0)
+    points = [p1, p2, p3, p4]
+
+    return Space(points=points, name=name)
+
+
+# =================
+# TESTS
+# =================
+
+def test_space_area(ten_by_ten_space):
+    """ten_by_ten_space should have an area value of 100"""
+    assert ten_by_ten_space.area == 100
+
+
+def test_place_contents_polygon(ten_by_ten_points, five_by_four_poly):
+    """Space object should be able to contain shapely Polygon objects"""
+    s = Space(points=ten_by_ten_points, contents=[five_by_four_poly])
+
+    assert s.plan['AREA: 20.0'].within(s.polygon)
+
+
+def test_place_contents_space(ten_by_ten_points, five_by_four_space):
+    """Space object should be able to contain shapely Polygon objects"""
+    s = Space(points=ten_by_ten_points, contents=[five_by_four_space])
+
+    assert s.plan['5x4 Space'].within(s.polygon)
+
+
+def test_place_contents_will_not_fit_second_content(ten_by_ten_points):
+    """Space object should be able to contain shapely Polygon objects"""
+    three_square = nameable_space('three square', 3, 3)
+    eight_square = nameable_space('eight square', 8, 8)
+
+    # eight_square is not expected to fit after three_square is placed
+    s = Space(points=ten_by_ten_points, contents=[
+        three_square, eight_square
+    ])
+
+    assert len(s.plan) == 1
+    assert list(s.plan.keys()) == ['three square']
+
+
+def test_place_contents_will_fit_second_content(ten_by_ten_points):
+    """Space object should be able to contain shapely Polygon objects"""
+    three_square = nameable_space('three square', 3, 3)
+    five_square = nameable_space('five square', 5, 5)
+
+    # eight_square is not expected to fit after three_square is placed
+    s = Space(points=ten_by_ten_points, contents=[
+        three_square, five_square
+    ])
+
+    assert len(s.plan) == 2
+    assert list(s.plan.keys()) == ['three square', 'five square']
+
+
+def test_plot_space(ten_by_ten_points, five_by_four_space):
+    """plt.show() should be called"""
+    s = Space(points=ten_by_ten_points, contents=[five_by_four_space])
+
+    with mock.patch('matplotlib.pyplot.show') as mock_show:
+        s.plot()
+        # plot_rooms([random_room])
+        mock_show.assert_called()
